4d09b5a9e28430e29570fe9c09e115a1f4e1ef41
Your Name <you@example.com>
2017-04-24 22:11:40 +0100

    1st commit

---
+++ botbase-module-phrases.gemspec
@@ -0,0 +1,18 @@
+Gem::Specification.new do |s|
+  s.name = 'botbase-module-phrases'
+  s.version = '0.1.0'
+  s.summary = 'A botbase module for matching a speech keyword which then ' + 
+      'triggers a general RSC job or publishes an SPS message etc.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/botbase-module-phrases.rb']
+  s.add_runtime_dependency('rsc', '~> 0.2', '>=0.2.2')
+  s.add_runtime_dependency('polyrex', '~> 1.1', '>=1.1.12')
+  s.add_runtime_dependency('sps-pub', '~> 0.5', '>=0.5.4')
+  s.add_runtime_dependency('spstrigger_execute', '~> 0.4', '>=0.4.6')
+  s.signing_key = '../privatekeys/botbase-module-phrases.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@jamesrobertson.eu'
+  s.homepage = 'https://github.com/jrobertson/botbase-module-phrases'
+end
+
---
+++ lib/botbase-module-phrases.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+
+# file: botbase-module-phrases.rb
+
+# A service module used by the BotBase gem
+
+
+require 'rsc'
+require 'polyrex' 
+require 'sps-pub'
+require 'spstrigger_execute'
+
+
+class BotBaseModulePhrases
+  
+  def initialize(host: nil, package_src: nil, reg: nil, keywords: '', 
+                 px_url: nil, sps_host: 'sps', sps_port: '59000', logfile: nil)
+
+    @rsc = RSC.new host, package_src
+     
+    #px = Polyrex.new px_url
+
+    @ste = SPSTriggerExecute.new keywords, reg=nil, px=nil, logfile: logfile
+    @keywords = keywords
+    @sps = SPSPub.new host: sps_host, port: sps_port
+
+  end
+  
+  def query(sender='user01', said)    
+        
+    #puts 'said: ' + said.inspect
+    
+    a = @ste.mae message: said
+
+    if a.length > 0 then
+      
+      h = {
+      
+        rse: ->(x, rsc){
+        
+          job = x.shift[/\/\/job:(.*)/,1]                  
+          package_path = x.shift 
+          package = package_path[/([^\/]+)\.rsf$/,1]
+          
+          rsc.run_job package, job, {}, args=x, package_path: package_path
+        }, 
+        sps: ->(x, rsc){ @sps.notice x },
+        ste: ->(x, rsc){ @ste.run x }
+      }
+
+    end
+            
+    a.each do |type, x| 
+      
+      Thread.new do
+        
+        begin
+          h[type].call x, @rsc
+        rescue
+          warning =  'botbase-module-phrases warning: ' + ($!).inspect
+          puts warning
+        end
+        
+      end # /thread
+      
+    end # /each           
+    
+  end
+  
+end
